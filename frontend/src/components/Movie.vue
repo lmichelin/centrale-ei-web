a1fa0d18562adb003880c4a0a0453ea5501a04c5
Aymeric Chaumont <aymeric.chaumont@student-cs.fr>
2022-06-08 13:51:51 +0200

    clickable films and cleaner catalogue

--- frontend/src/components/Movie.vue
+++ frontend/src/components/Movie.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="movie">
+  <router-link :to="'/movies/' + movie.id_" class="movie">
     <img
       :src="'https://image.tmdb.org/t/p/original' + movie.poster_path"
       width="230"
       class="poster"
     />
-    <div class="title">{{ movie.title }}</div>
-    <div class="date">Released {{ movie.release_date }}</div>
-  </div>
+    <div class="description">
+      <div class="title">{{ movie.title }}</div>
+      <div class="date">Released {{ movie.release_date }}</div>
+    </div>
+  </router-link>
 </template>
 
 <script>
@@ -21,21 +23,30 @@ export default {
 
 <style>
 .movie {
-  padding-top: 25px;
+  text-decoration: none;
+  margin-top: 25px;
   text-align: center;
   width: 230px;
 }
 
+.poster {
+  border: 2px solid #fff;
+  margin-bottom: 5 px;
+  border-radius: 15px;
+}
+.description {
+  background-color: black;
+  border: 2px solid #fff;
+  border-radius: 15px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+}
 .title {
-  color: orange;
+  color: white;
   -webkit-text-stroke: #000;
 }
 .date {
-  color: orange;
+  color: white;
   -webkit-text-stroke: #000;
-}
-.poster {
-  border: 1px solid #fff;
-  margin-bottom: 10px;
 }
 </style>
