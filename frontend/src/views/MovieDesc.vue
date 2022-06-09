e045cc9f2285dac3c03df4dce643da169577256d
Aymeric Chaumont <aymeric.chaumont@student-cs.fr>
2022-06-09 09:58:52 +0200

    linting

--- frontend/src/views/MovieDesc.vue
+++ frontend/src/views/MovieDesc.vue
@@ -1,42 +1,38 @@
 <template>
-<div class="MovieDesc">
-    <Movie :movie='movie' />
-</div>
+  <div class="MovieDesc">
+    <Movie :movie="movie" />
+  </div>
 </template>
-
 
 <script>
 import axios from "axios";
-import Movie from '@/components/Movie.vue';
+import Movie from "@/components/Movie.vue";
 export default {
-    name:"MovieDesc",
-    components: {Movie},
-    data: function() {
-        return {
-            movie: {}
-        }
+  name: "MovieDesc",
+  components: { Movie },
+  data: function () {
+    return {
+      movie: {},
+    };
+  },
+  methods: {
+    fetchTheMovie: function () {
+      axios
+        .get(`http://localhost:3000/movies/${this.$route.params.id}`)
+        .then((response) => {
+          this.movie = response.data[0];
+        })
+        .catch((error) => {
+          this.usersLoadingError = "An error occured while fetching the movie.";
+          console.error(error);
+        });
     },
-    methods: {
-        fetchTheMovie: function () {
-            axios
-            .get(`http://localhost:3000/movies/${this.$route.params.id}`)
-            .then ((response) => {
-            this.movie=response.data[0]
-            })
-            .catch((error) => {
-            this.usersLoadingError = "An error occured while fetching the movie.";
-            console.error(error);
-            });
-        }
-    },
-    mounted: function() {
-        console.log(this.$route)
-        this.fetchTheMovie();
-    }
-}
+  },
+  mounted: function () {
+    console.log(this.$route);
+    this.fetchTheMovie();
+  },
+};
 </script>
 
-
-<style>
-
-</style>
+<style></style>
